ac676a43da4b8db32bd5e10e81c362643ed053b6
Josh Reagan <jreagan@redhat.com>
2018-06-05 19:49:19 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Camel Spring Boot RabbitMQ
+
+To build this project use
+
+```
+$ mvn install
+```
+
+To run the project you can execute the following Maven goal
+
+```
+$ mvn spring-boot:run
+```
+
+You will also need an instance of RabbitMQ. I suggest you use a docker image. I used the following command:
+
+```
+$ docker run -d --hostname rabbitmq --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+```
---
+++ src/main/java/org/apache/camel/examples/CamelConfiguration.java
@@ -0,0 +1,40 @@
+package org.apache.camel.examples;
+
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelConfiguration extends RouteBuilder {
+
+  private static final Logger log = LoggerFactory.getLogger(CamelConfiguration.class);
+  
+  @Override
+  public void configure() throws Exception {
+    
+    restConfiguration("servlet")
+      .bindingMode(RestBindingMode.auto)
+      .contextPath("/camel")
+      .apiContextPath("/api-doc")
+    ;
+
+    rest("/events")
+      .post("/")
+        .consumes(MediaType.APPLICATION_JSON_VALUE)
+        .produces(MediaType.APPLICATION_JSON_VALUE)
+        .to("direct:post")
+    ;
+    
+    from("direct:post")
+      .routeId("handlePostIncidents")
+      .log(LoggingLevel.INFO, log, "Sending Message:\\n${body}")
+      .to(ExchangePattern.InOnly, "rabbitmq://{{camel.component.rabbitmq.host}}:{{camel.component.rabbitmq.port}}/events")
+      .setBody().constant(null)
+    ;
+  }
+}
